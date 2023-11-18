129865292351dac2ee2eccdda62c9a89e7343eba
OhItsLena <lena.hammerer@vol.at>
2023-11-18 23:51:24 +0100

    enhanced chatbot functionality

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,6 @@ from summarizer import LogSummarizer
 from question_answering import LogAIAgent
 import time
 import random
-st.set_page_config(page_title="Log2Win!")
 
 jokes = {"Why don't scientists trust atoms?":'Because they make up everything!', 
          'What did one wall say to the other wall?':"I'll meet you at the corner",
@@ -21,50 +20,68 @@ jokes = {"Why don't scientists trust atoms?":'Because they make up everything!',
          "How do you organize a space party?":"You planet!"
          }
 summary = ""
-upload_file = None
+uploaded_file = None
 
-summary = None
-
-def is_variable_initialized():
-    return summary is not None
+st.set_page_config(page_title="logai by Log2Win")
+st.markdown(
+    """
+    <style>
+    .sidebar .sidebar-content {
+        background-color: #your_color_code_here;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 with st.sidebar:
-    st.title('Log2Win App')
-
+    st.markdown(
+        """
+        <style>
+        .sidebar .sidebar-content {
+            background-color: #eaf2ef;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    logo_image = "Logo_vertikal.svg"
+    st.sidebar.image(logo_image, width=300)
     st.markdown('''
-    ## About
-    This app allows you to query different log files using natural language and get answers from a chatbot powered by our model.
+    ## About Me
+    I give your log files a voice. As an interactive assistant I allow you to gather insightful information by asking simple questions.
     ''')
+
     try:
         uploaded_file =  st.file_uploader(label="Upload your log file here")
     except:
         st.error("Could not parse Log File! Please try again ...")
 
+def get_summary(uploaded_file):
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    log_data = stringio.read()
+
+    logSummarizer = LogSummarizer(log_data=log_data[:3000])
+    summary = logSummarizer.summarize(10)
+    return summary
+
 if uploaded_file is not None:
-
-    with st.spinner("Preparing Log File..."):
-        try:
-            stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-            log_data = stringio.read()
-
-            logSummarizer = LogSummarizer(log_data=log_data[:10000])
-            summary = logSummarizer.summarize(10)   
-            while not is_variable_initialized():
-                key, val = random.choice(list(jokes.items()))
-                with st.chat_message("ai"):
-                    joke = (key + ": " + val )
-                    st.markdown(joke)
-                    time.sleep(7)
-
-            logAIAgent = LogAIAgent()
-
-        except:
-            st.error("Could not parse log data. Please provide a valid file.")
+    key, val = random.choice(list(jokes.items()))
+    joke = (key + " " + val)
+    with st.spinner(f'Preparing Log File...\n{joke}'):  
+        summary = get_summary(uploaded_file)
+        logAIAgent = LogAIAgent()
 
     with st.chat_message("ai"):
-        answer = f'**Here is a summary of the logs:**\n\n{summary}\n**What do you want to know?**'
-        st.markdown(answer)
-    #st.session_state.messages.append({"role": "ai", "content": answer})
+        st.markdown("**Here is a summary of the logs:**")
+        st.code(summary)
+        st.markdown("**What do you want to know?**")
+
+if 'details' not in st.session_state:
+    st.session_state.details = ''
+
+def set_details():
+    st.session_state.details = "DETAILS!!!!!"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -72,15 +89,22 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-# React to user input
+
 if prompt := st.chat_input("Ask Questions about the Log File!"):
-    # Display user message in chat message container
+    st.session_state.details = ''
     with st.chat_message("user"):
         st.markdown(prompt)
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     model_answer = logAIAgent.find_best_answer(summary,prompt)
+
     with st.chat_message("ai"):
-        output_answer = [st.markdown(model_answer), st.markdown("Would you like to see the detailed logs?"), st.button("Show Detailed Logs")]
-    st.session_state.messages.append({"role": "ai", "content": output_answer})
+        st.markdown(model_answer)
+        st.markdown("Would you like to see the detailed logs?")
+        st.button('Show Detailed Logs', on_click=set_details)
+    st.session_state.messages.append({"role": "ai", "content": model_answer})
+
+st.session_state.details
+
+
+
